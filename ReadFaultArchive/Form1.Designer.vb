a90f98efa46eb075780508faadd674dd0fb5edae
Giovanni Bernardi <bernyit@gmail.com>
2020-10-02 09:38:01 +0200

    Aggiungere i file di progetto.

---
+++ ReadFaultArchive/Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnRead = New System.Windows.Forms.Button()
+        Me.btnSelectOrigin = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.txtSource = New System.Windows.Forms.TextBox()
+        Me.btnSelectDestination = New System.Windows.Forms.Button()
+        Me.txtDestination = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog2 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.SuspendLayout()
+        '
+        'btnRead
+        '
+        Me.btnRead.Location = New System.Drawing.Point(287, 141)
+        Me.btnRead.Name = "btnRead"
+        Me.btnRead.Size = New System.Drawing.Size(333, 72)
+        Me.btnRead.TabIndex = 0
+        Me.btnRead.Text = "CONVERT"
+        Me.btnRead.UseVisualStyleBackColor = True
+        '
+        'btnSelectOrigin
+        '
+        Me.btnSelectOrigin.Location = New System.Drawing.Point(62, 32)
+        Me.btnSelectOrigin.Name = "btnSelectOrigin"
+        Me.btnSelectOrigin.Size = New System.Drawing.Size(176, 37)
+        Me.btnSelectOrigin.TabIndex = 1
+        Me.btnSelectOrigin.Text = "Select Wincc Fault File"
+        Me.btnSelectOrigin.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Filter = "WinCC Alarms|Faults_Archive*_HMI_Panel.txt|All Files|*.*"
+        '
+        'txtSource
+        '
+        Me.txtSource.Location = New System.Drawing.Point(244, 41)
+        Me.txtSource.Name = "txtSource"
+        Me.txtSource.Size = New System.Drawing.Size(531, 20)
+        Me.txtSource.TabIndex = 2
+        '
+        'btnSelectDestination
+        '
+        Me.btnSelectDestination.Location = New System.Drawing.Point(62, 75)
+        Me.btnSelectDestination.Name = "btnSelectDestination"
+        Me.btnSelectDestination.Size = New System.Drawing.Size(176, 37)
+        Me.btnSelectDestination.TabIndex = 3
+        Me.btnSelectDestination.Text = "Select Destination"
+        Me.btnSelectDestination.UseVisualStyleBackColor = True
+        '
+        'txtDestination
+        '
+        Me.txtDestination.Location = New System.Drawing.Point(244, 84)
+        Me.txtDestination.Name = "txtDestination"
+        Me.txtDestination.Size = New System.Drawing.Size(531, 20)
+        Me.txtDestination.TabIndex = 4
+        '
+        'OpenFileDialog2
+        '
+        Me.OpenFileDialog2.Filter = "CSV File|*.csv|All Files|*.*"
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.Filter = "CSV File|*.csv|All Files|*.*"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtDestination)
+        Me.Controls.Add(Me.btnSelectDestination)
+        Me.Controls.Add(Me.txtSource)
+        Me.Controls.Add(Me.btnSelectOrigin)
+        Me.Controls.Add(Me.btnRead)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnRead As Button
+    Friend WithEvents btnSelectOrigin As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents txtSource As TextBox
+    Friend WithEvents btnSelectDestination As Button
+    Friend WithEvents txtDestination As TextBox
+    Friend WithEvents OpenFileDialog2 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+End Class
---
+++ ReadFaultArchive/Form1.vb
@@ -0,0 +1,204 @@
+﻿Imports System.Data.OleDb
+Imports System.IO
+
+Public Class Form1
+
+    Dim countAlarmsOpen As Integer
+    Dim countAlarmsClosed As Integer
+
+
+
+
+    Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
+
+
+        Dim separator As Char = vbTab
+
+        Dim strfilename As String = "C:\ZZZZ_Chiavetta USB\Faults_Archive0_20190924_073636_HMI_Panel.txt"
+        Dim SR As StreamReader = New StreamReader(strfilename)
+        Dim filedata As String = SR.ReadToEnd
+
+        filedata = filedata.Replace(vbLf, "")
+        filedata = filedata.Replace("""", String.Empty)
+
+        Dim faultsWinccFormat = gio(filedata)
+
+        createStartEndAlarmList(faultsWinccFormat)
+
+
+        writeResultAlarmList()
+
+    End Sub
+
+    Structure strFaultsArchive
+        Dim ms As Double '0
+        Dim stateAFter As Int16 '2
+        Dim msgNumber As Int16 '4
+        Dim dateTimeString As String '13
+        Dim msgText As String '14
+    End Structure
+
+    Structure strFaultsList
+        Dim ms As Double
+        Dim msgNumber As Int16
+        Dim dateTimeStringBegin As String
+        Dim dateTimeStringEnd As String
+        Dim duration As Double
+        Dim msgText As String
+    End Structure
+
+
+    Public Function gio(ByVal fileData As String) As List(Of strFaultsArchive)
+
+        Dim rows = New List(Of strFaultsArchive)
+
+        Dim arrRow() As String = fileData.Split(Convert.ToChar(Keys.Return))
+
+        For Each row In arrRow
+            Dim rowStructured As strFaultsArchive
+            Dim separator As Char = vbTab
+
+            Dim arrWork() As String = row.Split(separator)
+
+            Try
+                rowStructured.ms = Double.Parse(arrWork(0)) / 1000
+                rowStructured.stateAFter = Int16.Parse(arrWork(2))
+                rowStructured.msgNumber = Int16.Parse(arrWork(4))
+                rowStructured.dateTimeString = arrWork(13)
+                rowStructured.msgText = arrWork(14)
+
+                rows.Add(rowStructured)
+            Catch ex As Exception
+
+            End Try
+
+        Next
+
+        Return rows
+
+    End Function
+
+
+    Dim newAlarmlist = New List(Of strFaultsList)
+
+    Private Sub createStartEndAlarmList(ByVal alarmsWincc As List(Of strFaultsArchive))
+
+
+        For Each alarmEvent In alarmsWincc
+            If alarmEvent.stateAFter = 1 Then
+                addAlarm(alarmEvent)
+                countAlarmsOpen += 1
+            Else
+                closeAlarm(alarmEvent)
+                countAlarmsClosed += 1
+            End If
+        Next
+
+    End Sub
+
+
+    Private Sub addAlarm(ByVal alarm As strFaultsArchive)
+
+        Dim newAlarm As strFaultsList
+
+        newAlarm.ms = alarm.ms
+        newAlarm.msgNumber = alarm.msgNumber
+        newAlarm.dateTimeStringBegin = alarm.dateTimeString
+        newAlarm.dateTimeStringEnd = ""
+        newAlarm.msgText = alarm.msgText
+        newAlarm.duration = -1
+        newAlarmlist.Add(newAlarm)
+
+    End Sub
+
+    Private Sub closeAlarm(ByVal alarm As strFaultsArchive)
+
+
+        For i As Integer = newAlarmlist.Count - 1 To 0 Step -1
+
+
+            If newAlarmlist(i).msgNumber = alarm.msgNumber Then
+
+                If newAlarmlist(i).dateTimeStringEnd = "" Then
+
+                    Dim newAlarm As strFaultsList
+
+                    newAlarm = newAlarmlist(i)
+                    newAlarm.dateTimeStringEnd = alarm.dateTimeString
+                    newAlarm.duration = timeCalculation(newAlarm.dateTimeStringBegin, newAlarm.dateTimeStringEnd)
+
+                    newAlarmlist(i) = newAlarm
+
+                End If
+
+
+
+                Exit For
+            End If
+
+        Next
+
+
+    End Sub
+
+
+    Private Function timeCalculation(ByVal startTime As String, ByVal endTime As String) As Double
+
+        Dim returnValue As Double = 0
+
+        Try
+            Dim startDate As DateTime = Convert.ToDateTime(startTime)
+            Dim endDate As DateTime = Convert.ToDateTime(endTime)
+            returnValue = DateDiff(DateInterval.Minute, startDate, endDate)
+        Catch ex As Exception
+
+        End Try
+
+        Return returnValue
+    End Function
+
+
+
+
+    Private Sub writeResultAlarmList()
+        Dim separator As String = ";"
+        Dim strfilename As String = "C:\ZZZZ_Chiavetta USB\Faults_Archive0_20190924_073636_HMI_Panel.csv"
+
+        Dim file As System.IO.StreamWriter
+        file = My.Computer.FileSystem.OpenTextFileWriter(strfilename, True)
+
+        For Each LINE As strFaultsList In newAlarmlist
+
+            Dim textLine As String
+
+            textLine = LINE.ms & separator &
+                        LINE.msgNumber & separator &
+                        LINE.dateTimeStringBegin & separator &
+                        LINE.dateTimeStringEnd & separator &
+                        LINE.duration & separator &
+                        LINE.msgText
+
+            file.WriteLine(textLine)
+
+        Next
+
+
+
+
+
+        file.Close()
+
+    End Sub
+
+    Private Sub btnSelectOrigin_Click(sender As Object, e As EventArgs) Handles btnSelectOrigin.Click
+
+        OpenFileDialog1.ShowDialog()
+
+        txtSource.Text = OpenFileDialog1.FileName
+    End Sub
+
+    Private Sub btnSelectDestination_Click(sender As Object, e As EventArgs) Handles btnSelectDestination.Click
+        SaveFileDialog1.ShowDialog()
+        txtDestination.Text = SaveFileDialog1.FileName
+    End Sub
+End Class
